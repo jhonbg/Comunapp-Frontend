e81e22996936a918bd78e0aa048bd2c1632ac4ed
JhonBG <jhon.botero1@udea.edu.co>
2024-09-18 22:30:46 -0500

    actualizacion de letra

--- src/pages/components/ModalError.tsx
+++ src/pages/components/ModalError.tsx
@@ -18,11 +18,11 @@ const ModalMessage: React.FC<ModalMessageProps> = ({ open, onClose, message }) =
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
+      <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4, color: 'black'}}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
           Mensaje
         </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        <Typography id="modal-modal-description" sx={{ mt: 2, color: 'black'}}>
           {message}
         </Typography>
         <Button onClick={onClose}>Cerrar</Button>
